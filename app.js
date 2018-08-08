ea099b496f6373d30d4c9a302cdb0d97fb2610ba
Sam Singer <samuel.singer1@gmail.com>
2018-08-08 00:04:25 -0700

    Game now has new game button and all other components of the game work

--- app.js
+++ app.js
@@ -29,45 +29,43 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     } 
     else {
         // Next player
-        alert(`You rolled a one. Player ${activePlayer} is up!`)
         document.querySelector('#current-' + activePlayer).textContent = '0';
-        document.querySelector('.player-0-panel').classList.toggle('active')
-        document.querySelector('.player-1-panel').classList.toggle('active')
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
         activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-        roundScore = 0;
-        
-        
-        
+        roundScore = 0;    
+        alert(`You rolled a one. Player ${activePlayer} is up!`);
     }
     
-})
+});
 
 document.querySelector('.btn-hold').addEventListener('click', function() {
-    // Take current roundScore and add to the score array
-    if (activePlayer === 0) {
-        score[0] += roundScore
-        document.getElementById('score-0').textContent = score[0];
-        
-        document.querySelector('#current-' + activePlayer).textContent = '0';
-        document.querySelector('.player-0-panel').classList.toggle('active')
-        document.querySelector('.player-1-panel').classList.toggle('active')
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-        roundScore = 0;
+    // Add CurrentScore to the Global Score
 
+    score[activePlayer] += roundScore;
+    
+    // Update the UI
+
+    document.getElementById('score-' + activePlayer).textContent = score[activePlayer];
+
+    // Check to see if the player has won
+
+    if (document.getElementById('score-' + activePlayer).textContent >= 10) {
+        alert(`Player ${activePlayer} is the winner!`);
+        location.reload();
     }
-    else {
-        score[1] += roundScore
-        document.getElementById('score-1').textContent = score[1];
 
-        document.querySelector('#current-' + activePlayer).textContent = '0';
-        document.querySelector('.player-0-panel').classList.toggle('active')
-        document.querySelector('.player-1-panel').classList.toggle('active')
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-        roundScore = 0;
+    // Next player
 
-    }
+    document.querySelector('#current-' + activePlayer).textContent = '0';
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+
+});
+
+document.querySelector('.btn-new').addEventListener('click', function() {
+    alert('A new game will begin');
+    location.reload();
 })
-
-// document.querySelector('#current-' + activePlayer).textContent = dice;
-// document.querySelector('#current-', + activePlayer).innerHTML = '<em>' + dice + '</em>';
-// let x = document.querySelector('#score-0').textContent
